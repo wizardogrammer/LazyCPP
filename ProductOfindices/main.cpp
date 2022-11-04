eac452472e058103a203e8d16f97eb41edeebd13
S1N4M <sinamoghimi73@gmail.com>
2022-11-04 23:38:23 +0300

    Update main.cpp

--- ProductOfindices/main.cpp
+++ ProductOfindices/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 
 void ProductOfIndices(const long* vec, int& N) {
-  long multiplication = 1;
-  for (int i = 0; i < N; i++) {
-    multiplication *= vec[i];
-    std::cout << multiplication << ",";
+  long multiplication{1};
+  char comma[3]{'\0', ' ', '\0'};
+
+  for (int i{}; i < N; i++) {
+    std::cout << comma << (multiplication *= vec[i]);
+    comma[0] = ',';
   }
+  std::cout << std::endl;
 }
 
 int main() {
-  int n;  // variable for getting number from user for size of array
+  // define variable `n` to get number from user for size of array
+  int n{};
   std::cout << " Enter the range of array : ";
   std::cin >> n;
-  long* arr = new long(n);  // dynamic array
-  for (int i = 0; i < n; i++) {
+
+  // Allocate memory for dynamic array `arr` in heap.
+  long* arr = new long(n);
+  for (int i{}; i < n; i++) {
     std::cout << " enter a number b(" << i + 1 << "): ";
     std::cin >> arr[i];
   }
 
   ProductOfIndices(arr, n);
+  delete arr;
+  return 0;
 }
