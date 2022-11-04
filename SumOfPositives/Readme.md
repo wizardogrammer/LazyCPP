e38f099b5f9a0b92916dd24f5a1fc7b2bcd549ed
S1N4M <sinamoghimi73@gmail.com>
2022-11-04 19:59:07 +0300

    Summation of positive numbers in a list

---
+++ SumOfPositives/Readme.md
@@ -0,0 +1,28 @@
+# Sum of Positives
+- 👋 Hi, we are @wizardogrammer, what a fantastic day!
+
+In this tutorial we show how you calculate the summation of positive numbers in a given list.
+
+`Input:` Enter your desired size of the list.
+
+`Second Input:` Enter the integer elements you want in the list one by one and then hit Enter.
+
+`Output:` The summation of positive numbers.
+
+## Example
+
+`Input:` 5
+
+`Second Input:` 
+
+                12
+
+                -15 
+
+                35 
+
+                -60 
+
+                -70
+
+`Output:` Final solution = 47
---
+++ SumOfPositives/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int sumPositive(const std::vector<int>& arr) {
+  int result{};
+  for (const int& num: arr)
+    if (num > 0) result += num;
+  return result;
+}
+
+int main() {
+  std::vector<int> real{};
+  unsigned num_elements{};
+  double element{};
+
+  std::cout << "Enter the number of element in the series: ";
+  std::cin >> num_elements;
+
+  // We get array elements and insert them to a vector via `pushback` method.
+  for (int i{1}; i <= num_elements; i++) {
+    std::cout << "Enter the element of the serie: ";
+    std::cin >> element;
+    real.push_back(element);
+  }
+
+  std::cout << "Final solution = " << sumPositive(real) << std::endl;
+}
