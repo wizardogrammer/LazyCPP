a1ccb189d19232f3fcf81a019775c5a11a99d859
S1N4M <sinamoghimi73@gmail.com>
2022-11-06 17:19:56 +0300

    Update main.cpp

--- SquareOfNumbers/main.cpp
+++ SquareOfNumbers/main.cpp
@@ -1,37 +1,37 @@
-#include<iostream>
-#include<vector>
-#include<cmath>
-
+#include <cmath>
+#include <iostream>
+#include <vector>
 
 // square of integers in a list function
-std::vector<int> squareOfNumber (std::vector<int> Arr, int N){
-	std::vector<int> result{};
-	for(int i = 0; i < Arr.size(); i++){
-		result.push_back(std::pow(Arr[i],2));
-	}
+std::vector<int> squareOfNumber(const std::vector<int>& Arr) {
+  std::vector<int> result(Arr.size());
 
-	return result;
+  for (int i = 0; i < Arr.size(); i++) result[i] = std::pow(Arr[i], 2);
+
+  return result;
 }
 
 // print a list function
-void printArray (std::vector<int> a){
-	for(int i = 0; i < a.size(); i++){
-		std::cout<<a[i]<<",";
-	}
+void printvec(const std::vector<int>& a) {
+  char comma[3]{'\0', ' ', '\0'};
+  for (const int& num : a) {
+    std::cout << comma << num;
+    comma[0] = ',';
+  }
+  std::cout << std::endl;
 }
 
-int main(){
-	int n;  //range of array.
-	std::cout<<"Enter the range of array n = ";
-	std::cin >> n;
-	std::vector<int> arr(n);
-	// creat a list by user.
-	for(int i = 0; i < n; i++){
-		std::cout<<"Enter a(" << i + 1 << ") = " ;
-		std::cin>> arr[i];
-	}
+int main() {
+  int n{};  // range of array.
+  std::cout << "Enter the range of array n = ";
+  std::cin >> n;
 
-	printArray (squareOfNumber(arr,n));	// call square and print function
+  std::vector<int> arr(n);
+  // creat a list by user.
+  for (int i{}; i < n; i++) {
+    std::cout << "Enter a(" << i + 1 << ") = ";
+    std::cin >> arr[i];
+  }
 
+  printvec(squareOfNumber(arr));  // call square and print function
 }
-
