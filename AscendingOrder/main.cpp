f3f27cd2a86169e93f3221d7e49b313d7a4d88ff
S1N4M <sinamoghimi73@gmail.com>
2022-11-06 17:19:48 +0300

    Update main.cpp

--- AscendingOrder/main.cpp
+++ AscendingOrder/main.cpp
@@ -1,29 +1,33 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
-std::vector<int> ascendingOrder(std::vector<int> arr, int N){
-    std::sort(arr.begin(), arr.end());
-    return arr;
+std::vector<int>& ascendingOrder(std::vector<int>& arr) {
+  std::sort(arr.begin(), arr.end());
+  return arr;
 }
 
-void printArray(std::vector<int> a){
-    for(int i = 0; i < a.size(); i++){
-        std::cout<< a[i] << ",";
-    }
+void printVec(const std::vector<int>& a) {
+  char comma[3]{'\0', ' ', '\0'};
+  for (const int& num : a) {
+    std::cout << comma << num;
+    comma[0] = ',';
+  }
+  std::cout << std::endl;
 }
 
-int main(){
-    int n;
-    std::cout<<"enter the range of array n = ";
-    std::cin >> n;
-    std::vector<int> arr(n);
-    for(int i = 0; i < n ; i++){
-        std::cout<<"Enter a("<< i+1 <<") : ";
-        std::cin>> arr[i];
-    }
+int main() {
+  int n{};
+  std::cout << "enter the length of array n = ";
+  std::cin >> n;
 
-    printArray(ascendingOrder(arr,n));
+  std::vector<int> arr(n);
+  for (int i{}; i < n; i++) {
+    std::cout << "Enter a(" << i + 1 << ") : ";
+    std::cin >> arr[i];
+  }
 
+  printVec(ascendingOrder(arr));
+
+  return 0;
 }
-
