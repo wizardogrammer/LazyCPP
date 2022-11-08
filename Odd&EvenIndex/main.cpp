f5168bf30d8b92c9b164f87453cf6cfc849efad8
S1N4M <sinamoghimi73@gmail.com>
2022-11-09 01:24:36 +0300

    Update main.cpp

--- Odd&EvenIndex/main.cpp
+++ Odd&EvenIndex/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> real(const std::vector<int>& n) {
+std::vector<int> real(const std::vector<int> &n) {
   std::vector<int> odd{}, even{};
 
-  for (const int& num : n)
+  for (const int &num : n)
     (num % 2 == 0) ? even.push_back(num) : odd.push_back(num);
 
   std::vector<int> result(odd);
@@ -13,9 +13,9 @@ std::vector<int> real(const std::vector<int>& n) {
   return result;
 }
 
-void printvec(const std::vector<int>& a) {
+void printvec(const std::vector<int> &a) {
   char comma[3]{'\0', ' ', '\0'};
-  for (const int& num : a) {
+  for (const int &num : a) {
     std::cout << comma << num;
     comma[0] = ',';
   }
